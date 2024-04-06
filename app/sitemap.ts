ba762e8e8664dd7f7a5c51763e3ebc311107c825
lyhou123 <lyhou282@gmail.com>
2024-04-06 16:12:05 +0700

    adding docker compose

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,19 +2,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://optimizing-seven.vercel.app/',
+      url: 'https://lyhou.sen-pai.live/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://optimizing-seven.vercel.app/about',
+      url: 'https://lyhou.sen-pai.live/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://optimizing-seven.vercel.app/service',
+      url: 'https://lyhou.sen-pai.live/products',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://lyhou.sen-pai.live/policy',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
